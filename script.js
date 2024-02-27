f32aaba1a120d329e76c39b4f641883ba7518d9e
Demetris Tzionis <demetristz@hotmail.com>
2024-02-27 19:55:32 +0700

    Increase game difficulty (1000), update reset button text and fix favicon display

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-'use strict';
+"use strict";
 
 // document.querySelector('.message').textContent = 'Correct Guess!';
 // document.querySelector('.number').textContent = 10;
 // document.querySelector('.score').textContent = 15;
 // document.querySelector('.guess').value = 1;
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = '?';
+const game_difficulty = 1000;
+let secretNumber = Math.trunc(Math.random() * game_difficulty) + 1;
+document.querySelector(".number").textContent = "?";
 let score = 20;
 let highscore = 0;
 
 // REFACTORING FUNCTIONS
 const displayScore = function () {
-  document.querySelector('.score').textContent = score;
+  document.querySelector(".score").textContent = score;
 };
 
 const displaySecretNumber = function (number) {
-  document.querySelector('.number').textContent = number;
+  document.querySelector(".number").textContent = number;
 };
 
 const changeBackgroundColor = function (color) {
-  document.querySelector('body').style.backgroundColor = color;
+  document.querySelector("body").style.backgroundColor = color;
 };
 
 const changeNumberWidth = function (width) {
-  document.querySelector('.number').style.width = width;
+  document.querySelector(".number").style.width = width;
 };
 
 const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+  document.querySelector(".message").textContent = message;
 };
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   // document.querySelector('.score').textContent = score;
   displayScore(); //REFACTORED VERSION
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  secretNumber = Math.trunc(Math.random() * game_difficulty) + 1;
   // document.querySelector('.number').textContent = '?';
-  displaySecretNumber('?'); //REFACTORED VERSION
+  displaySecretNumber("?"); //REFACTORED VERSION
   // document.querySelector('body').style.backgroundColor = '#222';
-  changeBackgroundColor('#222'); //REFACTORED VERSION
+  changeBackgroundColor("#222"); //REFACTORED VERSION
   // document.querySelector('.number').style.width = '15rem';
-  changeNumberWidth('15rem'); //REFACTORED VERSION
-  document.querySelector('.guess').value = '';
+  changeNumberWidth("15rem"); //REFACTORED VERSION
+  document.querySelector(".guess").value = "";
   // document.querySelector('.message').textContent = 'Start guessing...';
-  displayMessage('Start guessing...'); //REFACTORED VERSION
+  displayMessage("Start guessing..."); //REFACTORED VERSION
 });
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value); // CONVERT TO NUMBER TO HELP IMPLEMENTING INTO GAME LOGIC LATER ON
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value); // CONVERT TO NUMBER TO HELP IMPLEMENTING INTO GAME LOGIC LATER ON
 
   console.log(guess);
 
@@ -56,22 +57,22 @@ document.querySelector('.check').addEventListener('click', function () {
     // INSERTING A 0 OR AN INVALID INPUT CAUSES 0 WHICH IS A FALSY VALUE WITH AN OPPOSITE OF TRUTHY.
     // document.querySelector('.message').textContent =
     //   '** 0, empty or invalid input entered! **';
-    displayMessage('** 0, empty or invalid input entered! **'); //REFACTORED VERSION
+    displayMessage("** 0, empty or invalid input entered! **"); //REFACTORED VERSION
   }
 
   // guessing the correct number
   else if (guess === secretNumber) {
     // document.querySelector('.message').textContent = 'You found it!';
-    displayMessage('You found it'); //REFACTORED VERSION
+    displayMessage("You found it"); //REFACTORED VERSION
     // document.querySelector('body').style.backgroundColor = 'green';
-    changeBackgroundColor('green'); //REFACTORED VERSION
+    changeBackgroundColor("green"); //REFACTORED VERSION
     // document.querySelector('.number').style.width = '30rem';
-    changeNumberWidth('30rem'); //REFACTORED VERSION
+    changeNumberWidth("30rem"); //REFACTORED VERSION
     // document.querySelector('.number').textContent = secretNumber;
     displaySecretNumber(secretNumber); //REFACTORED VERSION
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      document.querySelector(".highscore").textContent = highscore;
     }
 
     //REFACTORED VERSION
@@ -79,13 +80,13 @@ document.querySelector('.check').addEventListener('click', function () {
     if (score > 1) {
       // document.querySelector('.message').textContent =
       //   guess > secretNumber ? 'Too high!' : 'Too low!';
-      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!'); //REFACTORED VERSION
+      displayMessage(guess > secretNumber ? "Too high!" : "Too low!"); //REFACTORED VERSION
       score--;
       // document.querySelector('.score').textContent = score;
       displayScore(); //REFACTORED VERSION
     } else {
       // document.querySelector('.message').textContent = 'GAME OVER';
-      displayMessage('GAME OVER'); //REFACTORED VERSION
+      displayMessage("GAME OVER"); //REFACTORED VERSION
       // document.querySelector('.score').textContent = 0;
       displayScore(0);
     }
